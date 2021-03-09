4c0d4af4013c019698aa6ff559b3486474f0425d
BoazWelsenis <D281029@edu.rocwb.nl>
2021-03-09 17:27:01 +0100

    Aanpassingen Website (StoringApp)
    Query uitvoeren werkend,
    
    Gebruiker terug sturen naar een pagina en melding laten zien / redirect,
    
    Dropdown maken (select + option),
    
    Checkbox versturen/ontvangen,
    
    Formulier afmaken,
    
    Validatie toevoegen aan de code door functies uit php te gebruiken, zodat er foutmelding getoond worden op het scherm wanneer er bijvoorbeeld iets niet is ingevuld. Wat eigenlijk wel nodig is.

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,16 +2,67 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
-$capaciteit = $_POST['capaciteit']; 
-$melder = $_POST['melder'];
+if(empty($attractie))
+{
+	$errors[] = "Vul de attractie-naam in.";
+}
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+$type = $_POST['type'];
+if(empty($type))
+{
+	$errors[] = "Vul het type in.";
+}
+
+$capaciteit = $_POST['capaciteit']; 
+if(!is_numeric($capaciteit))
+{
+	$errors[] = "Vul de capaciteit in.";
+}
+
+if(isset($_POST['prioriteit']))
+{
+	$prioriteit = true;
+}
+else{
+	$prioriteit = false;
+}
+
+$melder = $_POST['melder'];
+if(empty($melder))
+{
+	$errors[] = "Vul de naam van de melder in.";
+}
+
+$overig = $_POST['overig'];
+
+//Wanneer er een error is
+if(isset($errors))
+{
+	var_dump($errors);
+	die();
+}
+
+//Niet meer nodig wanneer je een redirect hebt toegepast met de header() functie
+//echo $attractie . " / " . $capaciteit . " / " . $melder;  
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info) VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+	":attractie" => $attractie,
+	":type" => $type,
+	":capaciteit" => $capaciteit,
+	":prioriteit" => $prioriteit,
+	":melder" => $melder,
+	":overige_info" => $overig,
+]);
+
+//Eigen response op POST request (ga terug naar index)
+header("Location: ../meldingen/index.php?msg=Melding opgeslagen");
--- meldingen/create.php
+++ meldingen/create.php
@@ -21,15 +21,34 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type" id="type">
+                    <option value="">- kies een type - </option>
+                    <option value="Achtbaan">Achtbaan</option>
+                    <option value="draaiend">Draaiend</option>
+                    <option value="kinder">Kinder</option>
+                    <option value="horeca">Horeca</option>
+                    <option value="show">Show</option>
+                    <option value="water">Water</option>
+                    <option value="overig">Overig</option>
+                </select>
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prio:</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit"><label for="prioriteit">Melding met prioriteit</label>
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+
+            <!-- JUISTE BENAMING NAME/ID? - QUERY + TEXTAREA -->
+            <div class="form-group">
+                <label for="overig">Overige info:</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
             
             <input type="submit" value="Verstuur melding">
